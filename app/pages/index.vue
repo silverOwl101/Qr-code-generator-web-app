5b31d57d1ef05ca849d59cde2f74efc085dcc3ef
silverOwl <contact.palucajh@gmail.com>
2026-02-05 21:53:22 +0800

    Add favicon, robots.txt, and TypeScript configuration files
    - Added a favicon.ico file to the public directory.
    - Created a robots.txt file to manage web crawler access.
    - Introduced a tsconfig.json file for TypeScript support in the project.

---
+++ app/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-gray-650">
+        <UCard class="w-full max-w-md shadow-xl border border-gray-800">
+            <template #header>
+                <div class="flex items-center justify-between">
+                    <span class="text-lg font-semibold text-white/60">QR Code Generator</span>
+                </div>
+            </template>
+            <div class="flex flex-col gap-6 py-4">
+                <UInput v-model="qrInput" placeholder="Enter text to generate QR code" size="lg" class="w-full"
+                    icon="i-heroicons-pencil-square" autofocus />
+                <div class="flex justify-center">
+                    <Qrcode :value="qrInput" class="w-48 h-48 rounded-lg border border-gray-200" />
+                </div>
+            </div>
+            <template #footer>
+                <div class="text-[10px] text-gray-500 text-center">Powered by Nuxt UI & QRCode</div>
+                <div class="text-xs text-gray-500 text-center">Made by John Harold Paluca</div>
+            </template>
+        </UCard>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const qrInput = ref('')
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
+    'nuxt-qrcode',
+  ],
+  css: ['~/assets/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ],
+  }
+})
